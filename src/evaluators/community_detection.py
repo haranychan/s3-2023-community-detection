397618e13da84cf83ee76ce0ff9b611a6ff26229
haranychan <nishihara-kei-jv@ynu.jp>
2023-07-16 20:30:29 +0900

    version 1.1 - modify the evaluator for community detection - the objective vaues of GRASP for regnier300-50, p500-100-1, and gauss500-100-1 were -254, -980, and -1203, respectively

--- src/evaluators/community_detection.py
+++ src/evaluators/community_detection.py
@@ -19,37 +19,71 @@ from dataclasses import dataclass
 import logging
 
 def evaluate(inputfile, outputfile):
-    n = int(inputfile.readline().strip())
-    w = []
-    for i in range(n):
-        aux = list(map(int, inputfile.readline().strip().split()))
-        assert len(aux) == n-i, (i, len(aux), n-i)
-        row = []
-        for j in range(i):
-            row.append(w[j][i])
-        row += aux
-        w.append(row)
+    # n = int(inputfile.readline().strip())
+    # w = []
+    # for i in range(n):
+    #     aux = list(map(int, inputfile.readline().strip().split()))
+    #     assert len(aux) == n-i, (i, len(aux), n-i)
+    #     row = []
+    #     for j in range(i):
+    #         row.append(w[j][i])
+    #     row += aux
+    #     w.append(row)
 
-    lines = outputfile.read().strip().split("\n")
-    cliques = []
-    nodes = set()
-    for line in lines:
-        try:
-            cliques.append(list(map(int, line.strip().split())))
-        except ValueError:
-            logging.warning(f"Ignoring line (failed to parse): {line}")
-            continue
-        for v in cliques[-1]:
-            if v > n or v < 1:
-                logging.error(f"Invalid node value {v}")
-                return None
-            if v in nodes:
-                logging.error(f"Repeated node value {v}")
-                return None
-            nodes.add(v)
-    if len(nodes) != n:
-        logging.error(f"Got {len(nodes)} unique nodes, expected {n}")
-        return None
+    with open(inputfile, "r") as file:
+        lines = file.readlines()
+        n = int(lines[0])
+        w: list[list[float]] = [[0 for j in range(n)] for i in range(n)]
+        t = 1
+        for i in range(n):
+            l = list(map(float, lines[t].split()))[1:]
+            for j in range(len(l)):
+                w[i][j] = l[j]
+                w[j][i] = l[j]
+            t += 1 
+
+    # lines = outputfile.read().strip().split("\n")
+    # cliques = []
+    # nodes = set()
+    # for line in lines:
+    #     try:
+    #         cliques.append(list(map(int, line.strip().split())))
+    #     except ValueError:
+    #         logging.warning(f"Ignoring line (failed to parse): {line}")
+    #         continue
+    #     for v in cliques[-1]:
+    #         if v > n or v < 1:
+    #             logging.error(f"Invalid node value {v}")
+    #             return None
+    #         if v in nodes:
+    #             logging.error(f"Repeated node value {v}")
+    #             return None
+    #         nodes.add(v)
+    # if len(nodes) != n:
+    #     logging.error(f"Got {len(nodes)} unique nodes, expected {n}")
+    #     return None
+    
+    with open(outputfile, "r") as file:
+        lines = file.readlines()
+        cliques = []
+        nodes = set()
+        for line in lines:
+            try:
+                cliques.append(list(map(int, line.split())))
+            except ValueError:
+                logging.warning(f"Ignoring line (failed to parse): {line}")
+                continue
+            for v in cliques[-1]:
+                if v > n - 1 or v < 0:
+                    logging.error(f"Invalid node value {v}")
+                    return None
+                if v in nodes:
+                    logging.error(f"Repeated node value {v}")
+                    return None
+                nodes.add(v)
+        if len(nodes) != n:
+            logging.error(f"Got {len(nodes)} unique nodes, expected {n}")
+            return None
 
     obj = 0
     for ind, c in enumerate(cliques):
@@ -66,14 +100,14 @@ if __name__ == '__main__':
     import sys
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('inputfile', type=argparse.FileType('r'))
-    parser.add_argument('outputfile', type=argparse.FileType('r'))
+    parser.add_argument('--input-file')
+    parser.add_argument('--output-file')
     args = parser.parse_args()
 
     logging.basicConfig(stream=sys.stderr, level="INFO",
                         format="%(levelname)s;%(message)s")
 
-    obj = evaluate(args.inputfile, args.outputfile)
+    obj = evaluate(args.input_file, args.output_file)
     print("Objective value:", obj)
 
 
